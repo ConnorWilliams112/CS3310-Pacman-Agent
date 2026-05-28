967e474eae0674175f22992da16c3d4584cb3578
ConnorWilliams112 <connor.williams@nps.edu>
2026-05-28 16:43:28 -0700

    Astar

--- search/search.py
+++ search/search.py
@@ -18,6 +18,7 @@ Pacman agents (in searchAgents.py).
 """
 
 import util
+from queue import PriorityQueue
 
 class SearchProblem:
     """
@@ -61,6 +62,69 @@ class SearchProblem:
         """
         util.raiseNotDefined()
 
+class Node:
+    """A node in a search tree. Contains a pointer to the parent (the node
+    that this is a successor of) and to the actual state for this node. Note
+    that if a state is arrived at by two paths, then there are two nodes with
+    the same state. Also includes the action that got us to this state, and
+    the total path_cost (also known as g) to reach the node. Other functions
+    may add an f and h value; see best_first_graph_search and astar_search for
+    an explanation of how the f and h values are handled. You will not need to
+    subclass this class."""
+
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        """Create a search tree Node, derived from a parent by an action."""
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
+
+    def __repr__(self):
+        return "<Node {}>".format(self.state)
+
+    def __lt__(self, node):
+        return self.state < node.state
+
+    def expand(self, problem):
+        """List the nodes reachable in one step from this node."""
+        return [self.child_node(problem, action)
+                for action in problem.actions(self.state)]
+
+    def child_node(self, problem, action):
+        """[Figure 3.10]"""
+        next_state = problem.result(self.state, action)
+        next_node = Node(next_state, self, action, problem.path_cost(self.path_cost, self.state, action, next_state))
+        return next_node
+
+    def solution(self):
+        """Return the sequence of actions to go from the root to this node."""
+        return [node.action for node in self.path()[1:]]
+
+    def path(self):
+        """Return a list of nodes forming the path from the root to this node."""
+        node, path_back = self, []
+        while node:
+            path_back.append(node)
+            node = node.parent
+        return list(reversed(path_back))
+
+    # We want for a queue of nodes in breadth_first_graph_search or
+    # astar_search to have no duplicated states, so we treat nodes
+    # with the same state as equal. [Problem: this may not be what you
+    # want in other contexts.]
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
+
+    def __hash__(self):
+        # We use the hash value of the state
+        # stored in the node instead of the node
+        # object itself to quickly search a node
+        # with the same state in a Hash Table
+        return hash(self.state)
 
 def tinyMazeSearch(problem):
     """
@@ -168,8 +232,35 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    frontier = PriorityQueue()
+    explored = set()
+    nodes_generated = 0
+    
+    initial_node = Node(problem.initial)
+    frontier.put((heuristic(initial_node, problem), initial_node))
+    nodes_generated += 1
+    
+    while frontier:
+        priority, node = frontier.get()
+        explored.add(node.state)
+        for child in node.expand(problem):
+            nodes_generated += 1
+            if child.state not in explored:
+                if problem.goal_test(child.state):
+                    print(f"Nodes generated: {nodes_generated}")
+                    return child
+                f = child.path_cost + heuristic(child, problem)
+                frontier.put((f, child))
+    print(f"Nodes generated: {nodes_generated}")
+    return None
 
+def h1(node, problem):
+    '''
+    The heuristic is the straight-line distance from a node's state to the goal.
+    '''
+    x1, y1 = node.state
+    x2, y2 = problem.goal
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 # Abbreviations
 bfs = breadthFirstSearch
