b5c34984a963efaf48849cc096b12e9dafc86faf
ConnorWilliams112 <connor.williams@nps.edu>
2026-05-31 19:49:45 -0700

    ++

--- search/search.py
+++ search/search.py
@@ -227,7 +227,6 @@ def nullHeuristic(state, problem=None):
 
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
     frontier = PriorityQueue()
     explored = set()
     
